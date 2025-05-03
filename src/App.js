fbdfaf4f97b744195298864a871fe0ef900f6298
jaav-ion <symphonicbiz@icloud.com>
2025-05-02 17:12:53 -0700

    UI refresh: added calming background and layout container

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@
 import { Analytics } from '@vercel/analytics/react';
 import React from 'react';
 import ReframeForm from './ReframeForm';
+import './App.css'; // make sure this is imported to apply styles
 
 function App() {
   return (
-    <div>
+    <div className="main-container">
       <ReframeForm />
       <Analytics />
     </div>
   );
 }
-
 
 export default App;
